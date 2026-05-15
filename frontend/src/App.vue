67efffe6356d8790ed9f44d539881517056c4c29
Developer <developer@demo.local>
2026-05-15 22:29:47 +0800

    chore: CHANGELOG自动更新至v1.3.4

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -23,7 +23,7 @@
 import { ref, onMounted } from 'vue';
 import DemoForm from './components/DemoForm.vue';
 
-const version = '1.3.3';
+const version = '1.3.4';
 const showChangelog = ref(false);
 const changelogHtml = ref('');
 
